fb2b00697751a9b3ab53b57c86b3283bbf2b20e9
JuanMend <juanmend690@gmail.com>
2020-03-08 23:55:12 -0500

    initial

---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'todo-list',
+	templateUrl: './todo-list.component.html',
+	styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
