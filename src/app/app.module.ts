0984eb084891bf93a86951bf827c015b8fbdbd3c
JuanMend <juanmend690@gmail.com>
2020-03-09 20:31:44 -0500

    updated TS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, TodoListComponent],
+	imports: [BrowserModule, AppRoutingModule, FormsModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -6,7 +6,41 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+	todos: object[];
+	todoTitle: string;
 	constructor() {}
 
-	ngOnInit(): void {}
+	ngOnInit() {
+		this.todoTitle = '';
+
+		this.todos = [
+			{
+				id: 1,
+				title: 'Eggs',
+				completed: false,
+				editing: false
+			},
+			{
+				id: 2,
+				title: 'Milk',
+				completed: false,
+				editing: false
+			},
+			{
+				id: 3,
+				title: 'Water',
+				completed: false,
+				editing: false
+			}
+		];
+	}
+
+	addTodo() {
+		this.todos.push({
+			id: 4,
+			title: this.todoTitle,
+			completed: false,
+			editing: false
+		});
+	}
 }
