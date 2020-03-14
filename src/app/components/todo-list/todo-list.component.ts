058832974a06978ce887109c987b2db05986e17c
JuanMend <juanmend690@gmail.com>
2020-03-14 13:15:24 -0500

    Updated

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from "@angular/core";
 import { Todo } from "../../interface/todo";
+import { trigger, transition, style, animate } from "@angular/animations";
 @Component({
   selector: "todo-list",
   templateUrl: "./todo-list.component.html",
-  styleUrls: ["./todo-list.component.css"]
+  styleUrls: ["./todo-list.component.css"],
+  animations: [
+    trigger("fade", [
+      transition(":enter", [
+        style({ opacity: 0, transform: "translateY(30px)" }),
+        animate(200, style({ opacity: 1, transform: "translateY(0px)" }))
+      ]),
+      transition(":leave", [
+        animate(200, style({ opacity: 0, transform: "translateY(30px)" }))
+      ])
+    ])
+  ]
 })
 export class TodoListComponent implements OnInit {
   todos: Todo[];
